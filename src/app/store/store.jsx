061d8601a2efe91fd8409b14b1212e85e4ea9e0b
Solomon Udofia <meetsolomonudofia@gmail.com>
2023-10-09 21:31:08 +0100

    corrected a file path in store.jsx

--- src/app/store/store.jsx
+++ src/app/store/store.jsx
@@ -3,7 +3,7 @@ import ecomReducer, { fetchData } from "../slice/slice";
 import cartReducer, { getTotal } from "../slice/cartSlice";
 import electReducer from "../slice/electSlice";
 import jewelReducer from "../slice/jewelSlice";
-import womenCReducer from "../slice/WomenCSlice";
+import womenCReducer from "../slice/womenCSlice";
 import menCReducer from "../slice/menCSlice";
 
 export const store = configureStore({
@@ -13,7 +13,7 @@ export const store = configureStore({
     elect: electReducer,
     jewel: jewelReducer,
     womenC: womenCReducer,
-    menC : menCReducer,
+    menC: menCReducer,
   },
 });
 
